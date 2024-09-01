48f2c6774dc0c15230ed89f1c35cd7a446a0ca93
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-31 22:32:55 -0400

    operaciones basicas

---
+++ OperacionesBasicas/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        // Suma
+        int c = a + b;
+        System.out.println("La suma es: " + c);
+
+        // Resta
+        int d = a - b;
+        System.out.println("La resta es: " + d);
+
+        // Multiplicar
+        int e = a * b;
+        System.out.println("La multiplicacion es: " + e);
+
+        // Division
+        int f = a / b;
+        System.out.println("La division es: " + f);
+
+        // Modulo
+        int g = a % b;
+        System.out.println("El modulo es: " + g);
+    }
+}
